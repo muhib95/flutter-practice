33429c27fb759ed133ca3ba6403592aba698a6f8
Muhibbul Hasan <34686084+muhib95@users.noreply.github.com>
2023-11-23 12:56:44 +0600

    title center

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,55 @@
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+// main(){
+//   runApp(const MyApp());
+// }
+//
+//
+// class MyApp extends StatelessWidget{
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(home:HomeActivity());
+//   }
+//
+// }
+//
+// class HomeActivity extends StatelessWidget{
+//   const HomeActivity({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//      appBar: AppBar(
+//        title: const Text ("Muhib",
+//        style: TextStyle(color: Colors.white
+//        ),
+//        ),
+//        backgroundColor: Colors.green,
+//
+//
+//      ),
+//     );
+//   }
+//
+// }
+
+
 import 'package:flutter/material.dart';
 
 main(){
   runApp(const MyApp());
 }
 
-
 class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home:HomeActivity());
+    // TODO: implement build
+ return const MaterialApp(home: HomeActivity());
   }
 
 }
@@ -21,10 +59,10 @@ class HomeActivity extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Muhib"),
-      ),
+      appBar: AppBar( title:const Center(child: Text("Hello",style: TextStyle(color: Colors.red))) ,
+      backgroundColor: Colors.green,),
     );
   }
 
